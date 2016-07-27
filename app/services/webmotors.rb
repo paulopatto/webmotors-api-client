67bbab7cd15a1e735d3bffabc0806891cde4ede2
Paulo Patto <paulopatto@paulopatto.com>
2016-07-27 18:22:00 -0300

    [webmotors_service]: Add validation returns success response

--- app/services/webmotors.rb
+++ app/services/webmotors.rb
@@ -6,13 +6,20 @@ module Service
 
     base_uri  "http://www.webmotors.com.br"
 
+    METHODS = {
+      makers: '/carro/marcas',
+      models: '/carro/modelos'
+    }
+
     def makers(options = {})
-      response = self.class.post('/carro/marcas', options)
+      response = self.class.post(METHODS[:makers], options)
+      return nil unless response.success?
       JSON.parse(response.body)
     end
 
     def models_from(maker_id)
-      response = self.class.post('/carro/modelos', { marca: maker_id })
+      response = self.class.post(METHODS[:models], { marca: maker_id })
+      return nil unless response.success?
       JSON.parse(response.body)
     end
   end
--- spec/services/webmotors_spec.rb
+++ spec/services/webmotors_spec.rb
@@ -7,16 +7,21 @@ describe Service::Webmotors do
   describe '#makers' do
     subject { described_class.new.makers }
 
-    it "returns json array with all makers" do
+    it 'returns json array with all makers' do
       expect(subject).to be_kind_of(Array)
     end
 
-    it "returned json array be include key :Nome" do
-      expect(subject.first).to be_include("Nome")
+    it 'returned json array be include key :Nome' do
+      expect(subject.first).to be_include('Nome')
     end
 
-    it "returned json array be include key :Id" do
-      expect(subject.first).to be_include("Id")
+    it 'returned json array be include key :Id' do
+      expect(subject.first).to be_include('Id')
+    end
+
+    it 'return nil if does not response with success' do
+      allow(described_class).to receive(:post).and_return(double(:response, success?: false))
+      expect(subject).to be_nil
     end
   end
 end
