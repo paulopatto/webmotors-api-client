d45875dcec02ae16a5224e3dd3b0e18d5c9aca52
Paulo Patto <paulopatto@paulopatto.com>
2016-07-28 16:28:51 -0300

    [add_coverage_specs]: annotated models

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: makes
+#
+#  id           :integer          not null, primary key
+#  name         :string
+#  webmotors_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Make < ActiveRecord::Base
   has_many :models, dependent: :destroy
 
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: models
+#
+#  id           :integer          not null, primary key
+#  make_id      :integer
+#  name         :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  webmotors_id :integer
+#
+
 class Model < ActiveRecord::Base
   belongs_to :make
 
